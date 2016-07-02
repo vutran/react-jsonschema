0165ca9ee3b6265d3253ce4f3299f93b7a2b6a7c
Vu Tran <vu@vu-tran.com>
2016-07-02 02:00:20 -0700

    updated example

--- examples/App.js
+++ examples/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { Form } from 'react-jsonschema';
+import React from 'react';
+import { Form } from 'react-jsonschema'; // eslint-disable-line import/no-unresolved
 import schema from './schema.json';
 import formData from './defaultData.json';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Form
-        schema={schema}
-        formData={formData}
-        onError={e => { console.log(e); } }
-        onSubmit={e => { console.log(e); } }
-      />
-    );
-  }
-}
+const log = msg => console.log(msg); // eslint-disable-line no-console
+
+const App = () => (
+  <Form
+    schema={schema}
+    formData={formData}
+    onError={e => { log(e); }}
+    onSubmit={e => { log(e); }}
+  />
+);
+
+export default App;
